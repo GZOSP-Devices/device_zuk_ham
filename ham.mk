7c721f68edd7480513182979e3a0ed2d3795892c
leodiram <sriramscience7@gmail.com>
2018-08-21 11:15:51 +0530

    Ham: Add wificond as our device needs it bugs: No Offload Service available
    Signed-off-by: leodiram <sriramscience7@gmail.com>

--- ham.mk
+++ ham.mk
@@ -262,6 +262,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libwpa_client \
     hostapd \
+    wificond \
     wpa_supplicant \
     wpa_supplicant.conf \
     wpa_supplicant_overlay.conf \
